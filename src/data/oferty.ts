821daee3220edae30466524696c523d89730c858
Marcin Smiechowicz <m.smiechowicz@hotmail.com>
2026-05-01 13:53:53 +0200

    First bunch of Blog posts + dynamic latest posts on index page + dynamic offer from the config file

---
+++ src/data/oferty.ts
@@ -0,0 +1,34 @@
+export const oferty = [
+    {
+        url: "https://freedom.pl/oferta/dzialka-na-sprzedaz-mszczonow-11177-3685-ogs/",
+        image: "/images/oferta_1.webp",
+        alt: "Siedlisko w okolicach Mszczonowa",
+        title: "Siedlisko w okolicach Mszczonowa",
+        location: "Siedlisko-Marków Towarzystw k/Mszczonowa",
+        price: "399 000 PLN"
+    },
+    {
+        url: "https://freedom.pl/oferta/mieszkanie-na-sprzedaz-pruszkow-przy-ksiezycu-52m2-41045-3685-oms/",
+        image: "/images/oferta_2.webp",
+        alt: "Mieszkanie w Pruszkowie przy ul. Przy Księżycu",
+        title: "Mieszkanie w Pruszkowie przy ul. Przy Księżycu",
+        location: "Pruszków, Mazowieckie",
+        price: "629 000 PLN"
+    },
+    {
+        url: "https://freedom.pl/oferta/dom-na-sprzedaz-stara-wies-89-3m2-17514-3685-ods/",
+        image: "/images/oferta_3.webp",
+        alt: "Bliźniak w Starej Wsi",
+        title: "Szereg w Starej Wsi",
+        location: "Stara Wieś, Mazowieckie",
+        price: "750 000 PLN"
+    },
+    {
+        url: "https://freedom.pl/oferta/dom-na-sprzedaz-rusiec-314m2-17516-3685-ods/",
+        image: "/images/oferta_4.webp",
+        alt: "Dom w Ruścu",
+        title: "Dom w Ruścu",
+        location: "Rusiec, Mazowieckie",
+        price: "1 990 000 PLN"
+    }
+];
