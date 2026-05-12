ed1ca7cbac8ac1948d62ee8b0f84116842e17547
Marcin Smiechowicz <m.smiechowicz@hotmail.com>
2026-05-12 10:04:12 +0200

    Improving index page linking to facebook

---
+++ src/data/socials.ts
@@ -0,0 +1,4 @@
+export const MESSENGER_URL = "https://m.me/61569082704747";
+export const FACEBOOK_URL = "https://www.facebook.com/profile.php?id=61569082704747";
+export const PHONE_NUMBER = "+48 576 670 050"; // Wersja do wyświetlania
+export const PHONE_CLEAN = "+48576670050"; // Wersja do linku (bez spacji)
