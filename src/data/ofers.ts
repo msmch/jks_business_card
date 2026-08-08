9401619e7cdf63c05c827739aa3814b3b1f312b5
Marcin Smiechowicz <m.smiechowicz@hotmail.com>
2026-08-08 13:54:25 +0200

    Removing offers

--- src/data/ofers.ts
+++ src/data/ofers.ts
@@ -1,42 +1,10 @@
 export const oferty = [
     {
-        url: "https://freedom.pl/oferta/dzialka-na-sprzedaz-mszczonow-11177-3685-ogs/",
+        url: "",
         image: "/images/oferta_1.webp",
-        alt: "Siedlisko w okolicach Mszczonowa",
-        title: "Siedlisko w okolicach Mszczonowa",
-        location: "Siedlisko-Marków Towarzystw k/Mszczonowa",
-        price: "399 000 PLN"
-    },
-    {
-        url: "https://freedom.pl/oferta/dom-na-sprzedaz-stara-wies-89-3m2-17514-3685-ods/",
-        image: "/images/oferta_3.webp",
-        alt: "Bliźniak w Starej Wsi",
-        title: "Szereg w Starej Wsi",
-        location: "Stara Wieś, Mazowieckie",
-        price: "750 000 PLN"
-    },
-    {
-        url: "https://freedom.pl/oferta/dom-na-sprzedaz-rusiec-314m2-17516-3685-ods/",
-        image: "/images/oferta_4.webp",
-        alt: "Dom w Ruścu",
-        title: "Dom w Ruścu",
-        location: "Rusiec, Mazowieckie",
-        price: "1 990 000 PLN"
-    },
-        {
-        url: "https://freedom.pl/oferta/dom-na-sprzedaz-kampinos-117-3m2-17933-3685-ods/",
-        image: "/images/oferta_5.webp",
-        alt: "Nowoczesny dom przy Kampinosie",
-        title: "Nowoczesny dom przy Kampinosie",
-        location: "Kampinos, Mazowieckie",
-        price: "879 000 PLN"
-    },
-    {
-        url: "https://freedom.pl/oferta/dom-na-sprzedaz-kampinos-124-5m2-17960-3685-ods/",
-        image: "/images/oferta_6.webp",
-        alt: "Nowoczesny dom przy Kampinosie",
-        title: "Nowoczesny dom przy Kampinosie",
-        location: "Kampinos, Mazowieckie",
-        price: "859 000 PLN"
-    },
+        alt: "",
+        title: "",
+        location: "",
+        price: ""
+    }
 ];
